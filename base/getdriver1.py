f394901d2ee40fb0f9c95d223b6e2364e430e3b6
hlunnnn <1217232263@qq.com>
2022-05-11 00:28:23 +0800

    111

--- base/getdriver1.py
+++ base/getdriver1.py
@@ -1,25 +1,20 @@
 from selenium import webdriver
 
-import page
-
 
 class GetDriver:
-    __driver=None
+    driver = None
 
     @classmethod
     def get_driver(cls):
         if cls.driver is None:
-            cls.driver=webdriver.Chrome()
+            cls.driver = webdriver.Chrome()
             cls.driver.maximize_window()
-            # cls.driver.implicitly_wait(5)
+
         return cls.driver
 
-
-    #关闭driver
     @classmethod
     def quit_driver(cls):
         if cls.driver:
             cls.driver.quit()
             cls.driver = None
-
 
--- script/suite.py
+++ script/suite.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     filepath = "../report/{}.html".format(time.strftime('%Y-%m-%d %H_%M_%S'))
 
     #定义测试报告的标题和描述
-    with open(filepath,'wb') as fp:
-        HTMLTestRunner(stream=fp,title='云培训项目自动化测试报告',description='操作系统：win10  谷歌浏览器').run(suite)
+    # with open(filepath,'wb') as fp:
+    #     HTMLTestRunner(stream=fp,title='云培训项目自动化测试报告',description='操作系统：win10  谷歌浏览器').run(suite)
 
-    # runner  = unittest.TextTestRunner()
-    # runner.run(suite)
+    runner  = unittest.TextTestRunner()
+    runner.run(suite)
 
